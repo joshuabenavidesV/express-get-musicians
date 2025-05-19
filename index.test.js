4aa3f6569dedbdf45ff4dce2c3d15773f84c3a13
Joshua Benavides <joshua.benavides@verizon.com>
2025-05-19 11:58:30 -0500

    wrote routes for muscians as a whole and individually, wrote test for them and did the same for bands

--- index.test.js
+++ index.test.js
@@ -12,8 +12,32 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
-    
-    
+test("Testing musicians endpoint", async () => {
+    // Sends request to `/muscians` endpoint
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+})
+test("Testing musicians indvidually", async () => {
+    // Sends request to `/musicians` endpoint
+    const response = await request(app).get("/musicians");
+    const responseData = JSON.parse(response.text);
+    expect(responseData[0].name).toBe("Mick Jagger")
+    expect(responseData[0].instrument).toBe("Voice")
+})
+
+test("Testing Bands endpoint", async () => {
+    // Sends request to `/muscians` endpoint
+    const response = await request(app).get("/bands");
+    expect(response.statusCode).toBe(200);
+})
+test("Testing Band individually", async () => {
+    // Sends request to `/musicians` endpoint
+    const response = await request(app).get("/bands");
+    const responseData = JSON.parse(response.text);
+    expect(responseData[0].name).toBe("The Beatles")
+    expect(responseData[1].name).toBe("Black Pink")
+    expect(responseData[2].name).toBe("Coldplay")
+})
 
 
 
--- src/app.js
+++ src/app.js
@@ -1,16 +1,43 @@
 const express = require("express");
 const app = express();
-const { Musician } = require("../models/index")
+const { Musician, Band } = require("../models/index")
 const { db } = require("../db/connection")
 
 const port = 3000;
 
 //TODO: Create a GET /musicians route to return all musicians 
 
-
-
-
-
-
+app.get("/musicians",async(request,response) => {
+    const musician = await Musician.findAll();
+    response.json(musician);
+})
+app.get("/musicians/1",async(request,response) => {
+    const musician1 = await Musician.findByPk(1);
+    response.json(musician1);
+})
+app.get("/musicians/2",async(request,response) => {
+    const musician2 = await Musician.findByPk(2);
+    response.json(musician2);
+})
+app.get("/musicians/3",async(request,response) => {
+    const musician3 = await Musician.findByPk(3);
+    response.json(musician3);
+})
+app.get("/bands",async(request,response) => {
+    const band = await Band.findAll();
+    response.json(band);
+})
+app.get("/bands/1",async(request,response) => {
+    const band = await Band.findByPk(1);
+    response.json(band);
+})
+app.get("/bands/2",async(request,response) => {
+    const band = await Band.findByPk(2);
+    response.json(band);
+})
+app.get("/bands/3",async(request,response) => {
+    const band = await Band.findByPk(3);
+    response.json(band);
+})
 
 module.exports = app;
