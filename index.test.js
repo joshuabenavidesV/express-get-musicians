f7859a6c2ab9d73684556b2d01fd9108ec610b6c
Joshua Benavides <joshua.benavides@verizon.com>
2025-05-20 12:03:28 -0500

    Added CRUD to muscians and wrote test for them

--- index.test.js
+++ index.test.js
@@ -12,6 +12,7 @@ const {seedMusician} = require("./seedData");
 
 describe('./musicians endpoint', () => {
     // Write your tests here
+    //Day 1 Test
 test("Testing musicians endpoint", async () => {
     // Sends request to `/muscians` endpoint
     const response = await request(app).get("/musicians");
@@ -38,9 +39,65 @@ test("Testing Band individually", async () => {
     expect(responseData[1].name).toBe("Black Pink")
     expect(responseData[2].name).toBe("Coldplay")
 })
+//Day 2 Test
+test("Testing musicians endpoint day 2", async () => {
+    // Sends request to `/muscians` endpoint
+    const response = await request(app).get("/musicians");
+    expect(response.statusCode).toBe(200);
+})
+test("Testing musicians with specific id", async () => {
+    // Sends request to `/musicians` endpoint
+    const response = await request(app).get("/musicians/2");
+    const responseData = JSON.parse(response.text);
+    expect(responseData.name).toBe("Drake")
+    expect(responseData.instrument).toBe("Voice")
+})
+// Test for POST(creating)
+test("Testing creating new musicians", async () => {
+    const newmusicians = {
+        name: 'Josh',
+        instrument: 'Guitar'
+    }
+    const response = await request(app).post("/musicians").send(newmusicians)
+    expect(response.body.name).toEqual("Josh")
+    expect(response.body.instrument).toEqual("Guitar")
+})
 
+//Test for PUT (updating)
+test("Testing updating new musicians", async () => {
+    let newMusician = {
+        name: 'Josh',
+        instrument: 'Guitar'
+    }
+     let response = await request(app)
+        .post("/musicians")
+        .send(newMusician);
+    let musicianId = response.body.id;
+    let updatedMusician = {
+        instrument: 'Bass'
+    }
+    let updatedResponse = await request(app)
+        .put(`/musicians/${musicianId}`)
+        .send(updatedMusician);
+    expect(updatedResponse.statusCode).toEqual(200);
+    expect(updatedResponse.body.instrument).toEqual("Bass");
+});
 
-
-
+// Test for delete (Deleting)
+test("Testing deleting new musicians", async () => {
+    let newMusician = {
+        name: 'Josh',
+        instrument: 'Guitar'
+    }
+     let response = await request(app)
+        .post("/musicians")
+        .send(newMusician);
+let musicianId = response.body.id;
+const deletedResponse = await request(app).delete(`/musicians/${musicianId}`)
+expect(deletedResponse.statusCode).toEqual(200);
+const fetchResponse = await request(app).get(`/musicians/${musicianId}`);
+    expect(fetchResponse.statusCode).toEqual(404);
+    
+});
     
 })
--- src/app.js
+++ src/app.js
@@ -5,39 +5,124 @@ const { db } = require("../db/connection")
 
 const port = 3000;
 
-//TODO: Create a GET /musicians route to return all musicians 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//TODO: Create a GET /musicians route to return all musicians 
+//Day 1 
 app.get("/musicians",async(request,response) => {
     const musician = await Musician.findAll();
     response.json(musician);
 })
-app.get("/musicians/1",async(request,response) => {
-    const musician1 = await Musician.findByPk(1);
-    response.json(musician1);
-})
-app.get("/musicians/2",async(request,response) => {
-    const musician2 = await Musician.findByPk(2);
-    response.json(musician2);
-})
-app.get("/musicians/3",async(request,response) => {
-    const musician3 = await Musician.findByPk(3);
-    response.json(musician3);
-})
-app.get("/bands",async(request,response) => {
-    const band = await Band.findAll();
+// app.get("/musicians/1",async(request,response) => {
+//     const musician1 = await Musician.findByPk(1);
+//     response.json(musician1);
+// })
+// app.get("/musicians/2",async(request,response) => {
+//     const musician2 = await Musician.findByPk(2);
+//     response.json(musician2);
+// })
+// app.get("/musicians/3",async(request,response) => {
+//     const musician3 = await Musician.findByPk(3);
+//     response.json(musician3);
+// })
+app.get("/bands",async(request,response,next) => {
+    try{
+        const band = await Band.findAll();
     response.json(band);
+    }
+    catch(error){
+        next(error);
+    }
 })
-app.get("/bands/1",async(request,response) => {
-    const band = await Band.findByPk(1);
-    response.json(band);
-})
-app.get("/bands/2",async(request,response) => {
-    const band = await Band.findByPk(2);
-    response.json(band);
-})
-app.get("/bands/3",async(request,response) => {
-    const band = await Band.findByPk(3);
-    response.json(band);
-})
+
+// app.get("/bands/1",async(request,response) => {
+//     const band = await Band.findByPk(1);
+//     response.json(band);
+// })
+// app.get("/bands/2",async(request,response) => {
+//     const band = await Band.findByPk(2);
+//     response.json(band);
+// })
+// app.get("/bands/3",async(request,response) => {
+//     const band = await Band.findByPk(3);
+//     response.json(band);
+// })
+
+// Day  2 
+
+// using to find specific musicians
+app.get("/musicians/:id", async (request, response) => {
+  const id = request.params.id;
+  const musician = await Musician.findByPk(id);
+  if (musician) {
+    response.json(musician);
+  } else {
+    response.status(404).json({ error_message: "Musician not found" });
+  }
+
+});
+// Post creating a new muscians
+app.post("/musicians", async (request, response, next) => {
+    try{
+    const newmusicians = await Musician.create(request.body);
+     response.status(201).json(newmusicians);
+    } catch(error){
+        next(error);
+    }
+});
+
+//Update musicians
+app.put("/musicians/:id", async (request, response) => {
+  const id = request.params.id;
+  await Musician.update(request.body, { where: { id } });
+  const updatedmusicians = await Musician.findByPk(id);
+  response.json(updatedmusicians);
+});
+// Deleting a musicians
+app.delete("/musicians/:id", async (request, response) => {
+  const id = request.params.id;
+  const deleted = await Musician.destroy({ where: { id } });
+  if (deleted) {
+    response.json({ message: "Musicians deleted" });
+  } else {
+    response.status(404).send("Musicians not found");
+  }
+});
+
+// Find band by specific id
+app.get("/bands/:id", async (request, response) => {
+  const id = request.params.id;
+  const bands = await Band.findByPk(id);
+  if (bands) {
+    response.json(bands);
+  } else {
+    response.status(404).json({ error_message: "Band not found" });
+  }
+});
+
+// Post creating a new Bands
+app.post("/bands", async (request, response) => {
+     const newbands = await Band.create(request.body);
+     response.json(newbands);
+});
+
+//Update band
+app.put("/bands/:id", async (request, response) => {
+  const id = request.params.id;
+  await Band.update(request.body, { where: { id } });
+  const updatedbands = await Band.findByPk(id);
+  response.json(updatedbands);
+});
+// Deleting a band
+app.delete("/bands/:id", async (request, response) => {
+  const id = request.params.id;
+  const deleted = await Band.destroy({ where: { id } });
+  if (deleted) {
+    response.json({ message: "Band deleted" });
+  } else {
+    response.status(404).json({ message: "Band not found" });
+  }
+});
 
 module.exports = app;
